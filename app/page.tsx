153c7cb0ebe6e296aafa778014f2d518c9b541cb
y1row <y1row.kato@gmail.com>
2025-10-11 15:11:32 +0900

    デスクトップとモバイルでアイコンを出し分ける

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from "@/app/ui/fonts";
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -28,7 +29,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="ダッシュボードプロジェクトのデスクトップ版を示すスクリーンショット"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="ダッシュボードプロジェクトのモバイル版を示すスクリーンショット"
+          />
         </div>
       </div>
     </main>
