415e519805d8d285718eedc195a1523d4912d7a9
y1row <y1row.kato@gmail.com>
2025-10-13 11:34:15 +0900

    `fetchCardData` を追加し、`Card` コンポーネントをダッシュボードページで有効化

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import { lusitana } from '@/app/ui/fonts';
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import { fetchLatestInvoices, fetchRevenue } from "@/app/lib/data";
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/app/lib/data";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { Card } from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
+  const cardData = await fetchCardData();
+
+  const totalPaidInvoices = cardData.totalPaidInvoices;
 
   return (
     <main>
@@ -14,7 +18,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
         {/* <Card
