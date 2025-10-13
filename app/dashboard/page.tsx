42537f42cca97e4a4103f7bc7900f98ee3c2b548
y1row <y1row.kato@gmail.com>
2025-10-13 11:42:23 +0900

    `totalPendingInvoices` を有効化して、`Pending` カードをダッシュボードに表示

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,6 +10,7 @@ export default async function Page() {
   const cardData = await fetchCardData();
 
   const totalPaidInvoices = cardData.totalPaidInvoices;
+  const totalPendingInvoices = cardData.totalPendingInvoices;
 
   return (
     <main>
@@ -19,7 +20,7 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
         {/* <Card
           title="Total Customers"
