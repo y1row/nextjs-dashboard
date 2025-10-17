6624f76587e3ba2bf745f848347c91189d2a9cb9
y1row <y1row.kato@gmail.com>
2025-10-17 10:37:11 +0900

    `Search` コンポーネントを追加し、`Invoices` ページをリファクタリング

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  function onChangeText(text: string) {
+    const params = new URLSearchParams(searchParams);
+    if (text) {
+      params.set('query', text);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathName}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +27,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event) => onChangeText(event.target.value)}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <MagnifyingGlassIcon
+        className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
