89e1755d23b577fd9b5be967900ae6c64756f2c5
y1row <y1row.kato@gmail.com>
2025-10-17 14:51:16 +0900

    `Pagination` コンポーネントを実装し、`Invoices` ページにページネーション機能を追加

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -15,6 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(text);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (text) {
       params.set('query', text);
     } else {
