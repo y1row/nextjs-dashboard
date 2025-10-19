226e015c5e8fef0b126a4667d420dc9dcc631599
y1row <y1row.kato@gmail.com>
2025-10-19 17:24:29 +0900

    `Invoices` ページに `Metadata` を追加し、メタデータを定義

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import InvoicesTable from "@/app/ui/invoices/table";
 import { fetchInvoicesPages } from "@/app/lib/data";
 import Pagination from "@/app/ui/invoices/pagination";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+};
 
 export default async function Page(props: {
   searchParams?: Promise<{
